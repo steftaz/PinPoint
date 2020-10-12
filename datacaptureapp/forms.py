2744868470e498e36c808b3bf72d83a49adc15af
Gijs Koehorst <g.s.koehorst@student.utwente.nl>
2020-10-12 15:43:28 +0200

    Added attributes to CreateProjectForm

--- datacaptureapp/forms.py
+++ datacaptureapp/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
 class CreateProjectForm(forms.Form):
-    project_name = forms.CharField(max_length=50, label='Project Name')
+    project_name = forms.CharField(max_length=50, label='Project Name', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter project name'
+        }))
     project_description = forms.CharField(widget=forms.Textarea, label='Description')
