325dad180a05805d7e206ee13c13f0591ef8eb04
Carlijn Meijerink <c.i.p.meijerink@student.utwente.nl>
2020-10-12 14:29:24 +0200

    template extending and right url connections

--- datacaptureapp/views.py
+++ datacaptureapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+def home(request):
+  return render(request, 'datacaptureapp/home.html', {})
 
-# def home(request):
-#     return render(request, 'templates/datacaptureapp/home.html', {})
+def newproject(request):
+    return render(request, 'datacaptureapp/NewProject.html', {})
+
+def project(request):
+    return render(request, 'datacaptureapp/Project.html',{})
+
+def addfeature(request):
+    return render(request, 'datacaptureapp/AddFeature.html', {})
+
+def featureoverview(request):
+    return render(request, 'datacaptureapp/FeatureOverview.html', {})
+
+def formcreation(request):
+    return render(request, 'datacaptureapp/FormCreation.html', {})
