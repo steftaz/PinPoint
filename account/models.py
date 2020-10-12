2fe9aeb26331e9651faa8ae475206a6302a4a872
Gijs Koehorst <g.s.koehorst@student.utwente.nl>
2020-10-12 10:40:18 +0200

    Added TestHtmlGeo.html which POSTS geolocation using Ajax

--- account/models.py
+++ account/models.py
@@ -39,12 +39,12 @@ class Account(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    objects = models.Manager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     object = MyAccountManager()
-
     def __str__(self):
         return self.email + ", " + self.username
 
--- datacaptureapp/views.py
+++ datacaptureapp/views.py
@@ -1,5 +1,15 @@
+import json
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# def home(request):
-#     return render(request, 'templates/datacaptureapp/home.html', {})
+def geolocation(request):
+    if request.method == 'POST':
+        latitude = request.POST.get("latitude")
+        longitude = request.POST.get("longitude")
+        response_data = {}
+        response_data['message'] = latitude
+
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+    else:
+        return render(request, "datacaptureapp/TestHtmlGeo.html")
