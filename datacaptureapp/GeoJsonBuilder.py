cf2d2c1053ac65e73a1b8dacb4e95f1b087367a7
Wouter Suidgeest <w.f.suidgeest@student.utwente.nl>
2020-10-12 21:00:43 +0200

    GeoJsonBuilder standalone done

--- datacaptureapp/GeoJsonBuilder.py
+++ datacaptureapp/GeoJsonBuilder.py
@@ -2,17 +2,35 @@ import os
 import django
 import json
 from manage import DEFAULT_SETTINGS_MODULE
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEFAULT_SETTINGS_MODULE)
 django.setup()
 from datacaptureapp.models import *
 
 
 def generate(name):
-
-    print(Project.objects.all())
-    print(Project.objects.first())
-    
-    return "whaa eind"
+    project = Project.objects.filter(name=name).first()
+    nodes = Node.objects.filter(project=project)
+    JSNodeList = []
+    for node in nodes:
+        JSNode = {}
+        JSNode["type"] = "Feature"
+        JSGeometry = {}
+        JSGeometry["type"] = "Point"
+        JSGeometry["coordinates"] = [float(node.latitude), float(node.longitude)]
+        JSNode["geometry"] = JSGeometry
+        datas = Data.objects.filter(node=node)
+        JSData = {}
+        for data in datas:
+            JSData[data.attribute.name] = data.value
+        JSNode["properties"] = JSData
+        JSNodeList.append(JSNode)
+    JSProject = {}
+    if project is not None:
+        JSProject["name"] = project.name
+    JSProject["type"] = "FeatureCollection"
+    JSProject["features"] = JSNodeList
+    return json.dumps(JSProject)
 
 
 print(generate("TestProject"))
