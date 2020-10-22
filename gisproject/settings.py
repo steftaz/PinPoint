685218f9820e4e855fb0c412d055d028362fe307
Wouter Suidgeest <w.f.suidgeest@student.utwente.nl>
2020-10-22 12:06:37 +0200

    Made edit/delete nodes better

--- gisproject/settings.py
+++ gisproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'o28_+gdwi98zv@&i6urdpm2fcd!2582@az^l%wdxewt=gb1!w2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.56.1', 'steftaz.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.56.1', 'steftaz.pythonanywhere.com', '192.168.0.103']
 STATIC_URL = '/static/'
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'datacaptureapp.apps.DatacaptureappConfig',
-    'account.apps.AccountConfig'
+    'account.apps.AccountConfig',
+    'sslserver'
 ]
 
 MIDDLEWARE = [
