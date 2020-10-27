65bc2225740822e26bb81b5b3894903c0f6c8190
Wouter Suidgeest <w.f.suidgeest@student.utwente.nl>
2020-10-27 13:48:43 +0100

    MEDIA_URL fixed

--- datacaptureapp/urls.py
+++ datacaptureapp/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('FAQ/', views.faq, name='faq')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
