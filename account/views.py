ed9ff73a2fa9c6ae0a758e7d73dc69c5ab4df17e
Steven Tazelaar <steven.tazelaar@gmail.com>
2020-10-28 17:20:58 +0100

    created first register capabilities

--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from account.forms import RegistrationForm
 
-# Create your views here.
+
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            print('successful account creation')
+            return redirect('projects')
+        else:
+            # add errors to context (form is not valid)
+            context['registration_form'] = form
+    else:
+        # GET request
+        form = RegistrationForm()
+        context['registration_form'] = form
+
+    return render(request, 'account/register.html', context)
+
+
--- gisproject/urls.py
+++ gisproject/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('datacaptureapp.urls')),
-    path('', include("django.contrib.auth.urls"))
+    path('', include("django.contrib.auth.urls")),
+    path('', include("account.urls"))
 ]
