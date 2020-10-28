b4c965b5b6c4d3c59ccfbc278750ce912a0a4813
Steven Tazelaar <steven.tazelaar@gmail.com>
2020-10-28 21:04:52 +0100

    setup new login

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 
 def registration_view(request):
@@ -26,3 +26,26 @@ def registration_view(request):
     return render(request, 'account/register.html', context)
 
 
+def login_view(request):
+    print("login")
+    context = {}
+
+    user = request.user
+    if user.is_authenticated:
+        return redirect("projects")
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("projects")
+    else:
+        form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+    return render(redirect, 'account/login.html', context)
