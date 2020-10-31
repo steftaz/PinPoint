9169e8f3a7859a138960054210c36e9a054c2f75
Carlijn Meijerink <c.i.p.meijerink@student.utwente.nl>
2020-10-31 15:48:38 +0100

    updating location

--- gisproject/settings.py
+++ gisproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o28_+gdwi98zv@&i6urdpm2fcd!2582@az^l%wdxewt=gb1!w2'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.56.1', 'steftaz.pythonanywhere.com', '192.168.0.103']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.56.1', 'steftaz.pythonanywhere.com', '192.168.0.103', '192.168.189.1']
 
 STATIC_URL = '/static/'
 
